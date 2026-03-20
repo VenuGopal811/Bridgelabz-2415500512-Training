627633483a7ff781940550254e319d48c35faca8
Alpha_VG <imvenugopal08@gmail.com>
2026-03-20 15:49:00 +0530

    Refactor nextGreatestFrequency method to return an array and update main method for output

--- Stack/nextGreatestFrequencyUsingStack.java
+++ Stack/nextGreatestFrequencyUsingStack.java
@@ -4,28 +4,30 @@ import java.util.Stack;
 class nextGreatestFrequencyUsingStack{
     public static void main(String[] args) {
         int[] arr = {2,1,1,3,2,1};
-        nextGreatestFrequency(arr);
+//        System.out.print(nextGreatestFrequency(arr));
+        int[] ans = nextGreatestFrequency(arr);
+        for (int x : ans) System.out.print(x + " ");
     }
 
-    public static void nextGreatestFrequency(int[] arr) {
+    public static int[] nextGreatestFrequency(int[] arr) {
         Stack<Integer> stack = new Stack<>();
-        int[] freq = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            freq[i] = 0;
-            for (int j = 0; j < arr.length; j++) {
+        int n = arr.length;
+        int[] freq = new int[n];
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (arr[i] == arr[j]) freq[i]++;
             }
         }
-        for (int i = 0; i < arr.length; i++) {
+
+        for (int i = n - 1; i >= 0; i--) {
             while (!stack.isEmpty() && freq[stack.peek()] <= freq[i]) {
                 stack.pop();
             }
-            if (stack.isEmpty()) {
-                System.out.print(-1 + " ");
-            } else {
-                System.out.print(arr[stack.peek()] + " ");
-            }
+            ans[i] = stack.isEmpty() ? -1 : arr[stack.peek()];
             stack.push(i);
         }
+        return ans;
     }
 }
