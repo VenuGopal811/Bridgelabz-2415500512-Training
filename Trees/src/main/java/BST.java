88f020267fb46ae1e8787d6892ef1786b2a22d47
Alpha_VG <imvenugopal08@gmail.com>
2026-04-14 14:39:03 +0530

    Remove package declaration from BST and BinaryTreeOrderTraversal classes

--- Trees/src/main/java/BST.java
+++ Trees/src/main/java/BST.java
@@ -1,4 +1,4 @@
-package main.java;
+
 
 public class BST {
     public static void main(String[] args) {
--- Trees/src/main/java/BinaryTreeOrderTraversal.java
+++ Trees/src/main/java/BinaryTreeOrderTraversal.java
@@ -1,5 +1,3 @@
-package main.java;
-
 import java.util.Queue;
 import java.util.LinkedList;
 import java.util.List;
